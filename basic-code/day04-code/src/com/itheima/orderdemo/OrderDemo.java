19e5bb987375e1030fe8cff7e30ad893a5c936e1
RUI6938 <165781824+RUI6938@users.noreply.github.com>
2025-02-28 23:02:26 +0800

    Add files via upload

---
+++ basic-code/day04-code/src/com/itheima/orderdemo/OrderDemo.java
@@ -0,0 +1,10 @@
+package com.itheima.orderdemo;
+
+public class OrderDemo {
+    public static void main(String[] args){
+        System.out.println("于光明中得救！永恒燃烧的羽翼,带你脱离凡间的沉沦");
+        System.out.println("一束花的仪式感");
+        System.out.println("人生是旷野不是轨道");
+        System.out.println("东风夜放花千树 更吹落 星如雨");
+    }
+}
---
+++ basic-code/day04-code/src/com/itheima/test/Test12.java
@@ -0,0 +1,23 @@
+package com.itheima.test;
+
+public class Test12 {
+    public static void main(String[] args){
+        //需求1：打印1-5
+        //分析
+        //开始条件：1
+        //结束条件：5
+        for(int i = 1;i <= 5;i++){
+            System.out.println(i);
+        }
+
+        //需求2：打印5-1
+        //分析：
+        //开始条件：5
+        //结束条件：1
+        for(int i = 5;i >= 1;i--){
+
+            System.out.println(i);
+        }
+
+    }
+}
