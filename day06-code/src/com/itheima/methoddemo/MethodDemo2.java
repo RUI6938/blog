aaaf7b882bb5dbd6bb73f34c9dabedcbdc4790f5
RUI6938 <165781824+RUI6938@users.noreply.github.com>
2025-02-28 22:48:46 +0800

    Add files via upload

---
+++ day06-code/src/com/itheima/methoddemo/MethodDemo2.java
@@ -0,0 +1,17 @@
+package com.itheima.methoddemo;
+
+public class MethodDemo2 {
+    public static void main(String[] args){
+        //目标：利用方法最简单的格式完成当前练习
+
+        //调用方法
+        printGFInfo();
+    }
+
+    public static void printGFInfo(){
+        System.out.println("小草莓");
+        System.out.println("清纯少女");
+        System.out.println("18岁");
+
+    }
+}
