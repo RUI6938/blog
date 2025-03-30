6ef7b6c56595abf228f4af17d6b6e01b859e87ee
RUI6938 <165781824+RUI6938@users.noreply.github.com>
2025-03-30 17:28:23 +0800

    Add files via upload

---
+++ mystring/src/com/heima/stringbuilderdemo/StringBuilderDemo3.java
@@ -0,0 +1,27 @@
+package com.heima.stringbuilderdemo;
+
+public class StringBuilderDemo3 {
+    public static void main(String[] args) {
+        //1.创建对象
+        StringBuilder sb = new StringBuilder("abc");
+
+        //2.添加元素
+        /*sb.append(1);
+        sb.append(2.3);
+        sb.append(true);*/
+
+        //反转内容
+        sb.reverse();
+
+        //获取长度
+        int len = sb.length();
+        System.out.println(len);
+
+        //打印
+        //普及：
+        //因为StringBuilder是java已经写好的类
+        //Java在底层对他做了一些特殊处理。
+        //打印对象不是地址值而是属性值。
+        System.out.println(sb);
+    }
+}
