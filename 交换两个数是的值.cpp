4628eebe4c66a61d9d44457b84bee14eb645d23b
RUI6938 <165781824+RUI6938@users.noreply.github.com>
2025-08-03 23:25:01 +0800

    Add files via upload

---
+++ 交换两个数是的值.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void Swap2(int* p1,int* p2)
+{
+	int tmp = 0;
+	tmp = *p1;//tmp = a
+	*p1 = *p2;//a = b
+	*p2 = tmp;//b = tmp
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d",&a,&b); 
+	printf("交换前： a = %d b = %d\n",a,b);
+	Swap2(&a,&b);
+	printf("交换后： a = %d b = %d\n",a,b);
+	return 0;
+}
